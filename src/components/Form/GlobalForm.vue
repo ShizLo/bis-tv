fea0c1d234192be68f8c6cfaa01f81575e33f6d9
Your Name <you@example.com>
2024-12-13 00:05:46 +0300

    Доработка обратной связи

--- src/components/Form/GlobalForm.vue
+++ src/components/Form/GlobalForm.vue
@@ -5,7 +5,6 @@ export default {
 };
 </script>
 <template>
-<<<<<<< HEAD
   <section class="global-form">
     <div class="_container global-form_mt">
       <div class="form-bl brd">
@@ -16,7 +15,6 @@ export default {
           <a class="link__phone phone" href="tel:+7 981 937-13-76">
             +7 981 999-99-96
           </a>
-
           <div class="flex-input">
             <div class="col">
               <input
@@ -26,33 +24,33 @@ export default {
                 id="input-name"
                 placeholder="Ваше имя"
               />
-=======
-    <section class="global-form">
-        <div class="_container global-form_mt">
-            <div class="form-bl brd">
-                <form class="form-submit" style="padding-bottom: 0;">
-                    <div class="form__title">Оставьте заявку</div>
-                    <input type="hidden" name="subject" value="Оставьте заявку">
-                    <p>Напишите нам свой вопрос или позвоните по номеру +7 981 999-99-96</p>
-                    <div class="flex-input">
-                        <div class="col">
-                            <input type="text" name="Ваше имя" value="" id="input-name" placeholder="Ваше имя">
-                        </div>
-                        <div class="col">
-                            <input type="email" name="Ваша почта" value="" placeholder="Ваша почта" id="input-email"
-                                required="">
-                        </div>
-                        <div class="col">
-                            <textarea name="Задайте ваш вопрос" placeholder="Задайте ваш вопрос"></textarea>
-                        </div>
-                        <input type="hidden" name="Ссылка источник" value="https://sewera.ru/contacts/">
-                    </div>
-                    <input class="form__button button_mtp" type="submit" value="Отправить">
-                </form>
->>>>>>> c46782076905e1160bf37e4d876cfcd62ea2b51c
             </div>
-        </div>
-    </section>
+            <div class="col">
+              <input
+                type="email"
+                name="Ваша почта"
+                value=""
+                placeholder="Ваша почта"
+                id="input-email"
+                required=""
+              />
+            </div>
+            <div class="col">
+              <textarea
+                name="Задайте ваш вопрос"
+                placeholder="Задайте ваш вопрос"
+              ></textarea>
+            </div>
+          </div>
+          <input
+            class="form__button button_mtp"
+            type="submit"
+            value="Отправить"
+          />
+        </form>
+      </div>
+    </div>
+  </section>
 </template>
 <style lang="scss" coped>
 @use "../../assets/styles/app.scss" as c;
@@ -107,7 +105,7 @@ textarea {
   vertical-align: top;
   padding: 10px 0 4px;
   border: none;
-  border-bottom: 2px solid #102938;
+  border-bottom: 1px solid #102938;
   background: 0 0;
   -moz-border-radius: 0;
   -webkit-border-radius: 0;
