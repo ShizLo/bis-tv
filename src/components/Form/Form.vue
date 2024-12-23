d2a8a59e5d3c7565fcb42b3a08a6815587761145
Alex_Tryufanov <tryufanov@bk.ru>
2024-12-23 22:50:43 +0200

    Форма - модальное окно

--- src/components/Form/Form.vue
+++ src/components/Form/Form.vue
@@ -1,4 +1,6 @@
 <script>
+
+
 export default {
   props: {},
   computed: {},
@@ -31,8 +33,8 @@ export default {
 };
 </script>
 <template>
-  <div class="feedback">
-    <div class="feedback__container">
+  <div class="containers">
+    <div class="reveal-modal">
       <button class="feedback__close-btn" @click="$emit('someEvent')">
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
@@ -70,6 +72,92 @@ export default {
   </div>
 </template>
 <style lang="scss" scoped>
+.containers {
+    width: 100%;
+    height: 100%;
+    top: 0;
+    position: absolute;
+    // visibility: hidden;
+    display: none;
+    background-color: rgba(22,22,22,0.5); /* complimenting your modal colors */
+}
+.reveal-modal {
+  display: flex;
+    position: relative;
+    /* margin-bottom: 10px; */
+    padding-bottom: 50px;
+    margin: 0 auto;
+    top: 30%;
+    background-color: white;
+    width: 35%;
+    flex-direction: column;
+    align-content: center;
+    align-items: center;
+    padding-top: 50px;
+    z-index: 100;
+    border-radius: 10px;
+}
+.modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    background: #FFFFFF;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .modal-header,
+  .modal-footer {
+    padding: 15px;
+    display: flex;
+  }
+
+  .modal-header {
+    border-bottom: 1px solid #eeeeee;
+    color: #4AAE9B;
+    justify-content: space-between;
+  }
+
+  .modal-footer {
+    border-top: 1px solid #eeeeee;
+    justify-content: flex-end;
+  }
+
+  .modal-body {
+    position: relative;
+    padding: 20px 10px;
+  }
+
+  .btn-close {
+    border: none;
+    font-size: 20px;
+    padding: 20px;
+    cursor: pointer;
+    font-weight: bold;
+    color: #4AAE9B;
+    background: transparent;
+  }
+
+  .btn-green {
+    color: white;
+    background: #4AAE9B;
+    border: 1px solid #4AAE9B;
+    border-radius: 2px;
+  }
+  ///////////////////////////////////////////////////////
+
+
 .feedback {
   position: fixed;
   top: 0;
