cdc683e93ae7cf3474248eee8cf47507c45c81e3
Alex_Tryufanov <tryufanov@bk.ru>
2025-01-31 01:15:30 +0200

    MiniWorksSlider внеснение изменений

--- src/components/MiniWorksSlider.vue
+++ src/components/MiniWorksSlider.vue
@@ -43,6 +43,11 @@ export default {
         el: ".swiper-pagination1",
         clickable: true,
         type: "fraction",
+        renderFraction: function (currentClass, totalClass) {
+      return '<span class="">фото </span>'+'<span class="' + currentClass + '"></span>' +
+              ' из ' +
+              '<span class="' + totalClass + '"></span>';
+  }
       },
       
       thumbs: {
@@ -61,15 +66,14 @@ export default {
 <template>
   <div class="mini-slider__backgroud" @click.self="$emit('someEvent')">
     <button class="form-feedback__close-btn" @click="$emit('someEvent')">
-      <svg width="30" height="30" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1.20868 14.5405L14.2902 1.45898M14.2902 14.5405L1.20875 1.45899" stroke="#f5f5f5" stroke-width="2"
-          stroke-linecap="round"></path>
-      </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" stroke="#f5f5f5" viewBox="0 0 50 50" width="50px" height="50px">
+        <path fill="#f5f5f5" d="M 25 2 C 12.309534 2 2 12.309534 2 25 C 2 37.690466 12.309534 48 25 48 C 37.690466 48 48 37.690466 48 25 C 48 12.309534 37.690466 2 25 2 z M 25 4 C 36.609534 4 46 13.390466 46 25 C 46 36.609534 36.609534 46 25 46 C 13.390466 46 4 36.609534 4 25 C 4 13.390466 13.390466 4 25 4 z M 32.990234 15.986328 A 1.0001 1.0001 0 0 0 32.292969 16.292969 L 25 23.585938 L 17.707031 16.292969 A 1.0001 1.0001 0 0 0 16.990234 15.990234 A 1.0001 1.0001 0 0 0 16.292969 17.707031 L 23.585938 25 L 16.292969 32.292969 A 1.0001 1.0001 0 1 0 17.707031 33.707031 L 25 26.414062 L 32.292969 33.707031 A 1.0001 1.0001 0 1 0 33.707031 32.292969 L 26.414062 25 L 33.707031 17.707031 A 1.0001 1.0001 0 0 0 32.990234 15.986328 z"/></svg>
     </button>
     <div class="mini-slider__container">
       <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff"class="swiper-container mySwipers2">
         <div class=" swiper-wrapper">
           <div class="swiper-slide" v-for="item in workImages" :key="item">
+            <p class="swiper-slide__text">{{ item.alt }}</p>
             <img class="resize" :src=item.url :alt="item.alt" />
           </div>
         </div>
@@ -91,7 +95,7 @@ export default {
 @use "../assets/styles/main.scss" as *;
 .form-feedback__close-btn {
   position: absolute;
-  right: 25px;
+  right: 50px;
   top: 10px;
   width: 16px;
   height: 16px;
@@ -103,7 +107,13 @@ export default {
 .form-feedback__close-btn svg {
   filter: drop-shadow(1px 0px 9px black);
 }
-
+.swiper-slide__text {
+  position: absolute;
+    top: 90%;
+    box-shadow: 0px 22px 53px 22px rgba(0, 0, 0, 0.72);
+    color: #f5f5f5;
+  background-color: rgba(0, 0, 0, 0.455);
+}
 .mini-slider__container {
   display: flex;
   flex-direction: column;
@@ -131,6 +141,13 @@ export default {
 };
 .swiper-button-prev1, .swiper-button-next1{
   display: none;
+};
+.mySwipers2 {
+  position: absolute;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
 }
   };
 
@@ -205,8 +222,14 @@ export default {
   font-size: 1rem;
   color: #f5f5f5;
   bottom: 2px;
-  width: 30px;
-  margin: 0px 50%;
+  width: 100px;
+  justify-content: space-evenly;
+  position: absolute;
+    top: 97%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  background-color: rgba(0, 0, 0, 0.455);
+  box-shadow: 0px 22px 53px 22px rgba(0, 0, 0, 0.72);
 }
 
 .mini-slider__backgroud {
